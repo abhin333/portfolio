93cf1f2db1992d413732ac3e156265b90f65abd6
abhin333 <abhinpradeepan123@gmail.com>
2023-06-27 01:27:45 -0700

    "add new updates"

--- src/Component/Portfolio/Portfolio.jsx
+++ src/Component/Portfolio/Portfolio.jsx
@@ -5,7 +5,7 @@ import IMG2 from "../../assets/portfolio2.png";
 import IMG3 from "../../assets/portfolio3.png";
 import IMG4 from "../../assets/portfolio4.jpg";
 import IMG5 from "../../assets/portfolio5.jpg";
-import IMG6 from "../../assets/portfolio6.jpg";
+import IMG6 from "../../assets/movie-logo.jpg";
 
 const Portfolio = () => {
   return (
@@ -14,50 +14,7 @@ const Portfolio = () => {
         <h5>My Recent Work</h5>
         <h2>portfolio</h2>
         <div className="container portfolio__container">
-          <article className="portfolio__item">
-            <div className="portfolio__item-image">
-              <img src={IMG1} alt="image1" />
-            </div>
-            <h3>This is a Handloom </h3>
-            <div className="portfolio__item-cta">
-              <a
-                href="https://github.com/abhin333/hanveev.git"
-                className="btn"
-                target="_blank"
-              >
-                Github
-              </a>
-              <a
-                href="https://hanveev.pythonanywhere.com/"
-                className="btn btn-primary"
-                target="_blank"
-              >
-                Live Demo
-              </a>
-            </div>
-          </article>
-          <article className="portfolio__item">
-            <div className="portfolio__item-image">
-              <img src={IMG2} alt="image2" />
-            </div>
-            <h3>This is a Camera rent </h3>
-            <div className="portfolio__item-cta">
-              <a
-                href="https://github.com/abhin333/ecommerce.git"
-                className="btn"
-                target="_blank"
-              >
-                Github
-              </a>
-              <a
-                href="https://camerarentnew.pythonanywhere.com/"
-                className="btn btn-primary"
-                target="_blank"
-              >
-                Live Demo
-              </a>
-            </div>
-          </article>
+          
           <article className="portfolio__item">
             <div className="portfolio__item-image">
               <img src={IMG3} alt="image3" />
@@ -106,6 +63,73 @@ const Portfolio = () => {
 
           <article className="portfolio__item">
             <div className="portfolio__item-image">
+              <img src={IMG6} alt="image6" />
+            </div>
+            <h3>Movie App</h3>
+            <div className="portfolio__item-cta">
+              <a
+                href="https://github.com/abhin333/movies"
+                className="btn"
+                target="_blank"
+              >
+                Github
+              </a>
+              <a
+                href="https://movie-cuchufli-7c35bc.netlify.app/"
+                className="btn btn-primary"
+                target="_blank"
+              >
+                Live Demo
+              </a>
+            </div>
+          </article>
+          <article className="portfolio__item">
+            <div className="portfolio__item-image">
+              <img src={IMG1} alt="image1" />
+            </div>
+            <h3>This is a Handloom </h3>
+            <div className="portfolio__item-cta">
+              <a
+                href="https://github.com/abhin333/hanveev.git"
+                className="btn"
+                target="_blank"
+              >
+                Github
+              </a>
+              <a
+                href="https://hanveev.pythonanywhere.com/"
+                className="btn btn-primary"
+                target="_blank"
+              >
+                Live Demo
+              </a>
+            </div>
+          </article>
+          <article className="portfolio__item">
+            <div className="portfolio__item-image">
+              <img src={IMG2} alt="image2" />
+            </div>
+            <h3>This is a Camera rent </h3>
+            <div className="portfolio__item-cta">
+              <a
+                href="https://github.com/abhin333/ecommerce.git"
+                className="btn"
+                target="_blank"
+              >
+                Github
+              </a>
+              <a
+                href="https://camerarentnew.pythonanywhere.com/"
+                className="btn btn-primary"
+                target="_blank"
+              >
+                Live Demo
+              </a>
+            </div>
+          </article>
+
+          <article className="portfolio__item">
+            <div className="portfolio__item-image">
               <img src={IMG5} alt="image5" />
             </div>
             <h3>Facebook clone</h3>
@@ -127,28 +151,7 @@ const Portfolio = () => {
             </div>
           </article>
 
-          <article className="portfolio__item">
-            <div className="portfolio__item-image">
-              <img src={IMG6} alt="image6" />
-            </div>
-            <h3>This is demo</h3>
-            <div className="portfolio__item-cta">
-              <a
-                href="https://github.com/abhin333"
-                className="btn"
-                target="_blank"
-              >
-                Github
-              </a>
-              <a
-                href="https://abhin333.github.io/abhin/"
-                className="btn btn-primary"
-                target="_blank"
-              >
-                Live Demo
-              </a>
-            </div>
-          </article>
+          
         </div>
       </section>
     </>
